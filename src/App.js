e6edd12ea5e538dcc2d283f2105c5fb1a9cea598
bel0604 <162352229+bel0604@users.noreply.github.com>
2024-06-08 15:52:35 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Membro from './components/';
+
+class App extends Component{
+
+  render(){
+    return(
+      <div>
+        <Membro nome = "vistante"/>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+ class Membro extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            nome:props.nome
+        };
+        this.entrar = this.entrar.bind(this);
+    }
+
+    entrar(){
+      this.setState({Nome: 'Matheus'})
+    }
+ 
+    render(){
+        return(
+            <div>
+               <h2>Bem Vindo!{this.state.nome}</h2>
+               <button onClick={this.entrar}>
+                Entrar com Matheus
+               </button>
+               <button onClick={ () => this.setState({nome:''})}>
+                Sair
+               </button>
+            </div>
+        );
+    }
+}
+export default Membro; 
